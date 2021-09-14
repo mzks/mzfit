15c16d57d9554aed79eb032a0daf1d06e20038d3
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2021-09-14 14:07:22 +0900

    Adopt python 3.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=requires,
     license="MIT",
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
